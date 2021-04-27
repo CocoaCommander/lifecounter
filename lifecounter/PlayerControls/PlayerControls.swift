a5f1fd4b51c065c480a8a46da483b969cc116dab
Ryan Lee <ralee478.school@gmail.com>
2021-04-27 13:50:28 -0700

    added history/add player buttons

---
+++ lifecounter/PlayerControls/PlayerControls.swift
@@ -0,0 +1,19 @@
+//
+//  PlayerControls.swift
+//  lifecounter
+//
+//  Created by Ryan Lee on 4/27/21.
+//
+
+import UIKit
+
+class PlayerControls: UIView {
+    @IBOutlet var controls: UIView!
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        
+    }
+    
+}
