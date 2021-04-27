311a17a0e94647925eeef3b3a523fd48736c9e57
Ryan Lee <ralee478.school@gmail.com>
2021-04-26 19:47:17 -0700

    I give up

---
+++ lifecounter/PlayerCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PlayerCollectionViewCell.swift
+//  lifecounter
+//
+//  Created by Ryan Lee on 4/26/21.
+//
+
+import UIKit
+
+class PlayerCollectionViewCell: UICollectionViewCell {
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
